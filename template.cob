2a35b5bb4edf2a4cb6924be99fa65276a92dc77e
Andrew Young <andrew.young@mongodb.com>
2016-12-08 10:40:20 -0600

    Removed whitespace

--- template.cob
+++ template.cob
@@ -1,39 +1,39 @@
-000100* The MIT License (MIT)                                                   
-000200*                                                                         
-000300* Copyright (c) 2015 Andrew Young                                         
-000400*                                                                         
-000500* Permission is hereby granted, free of charge, to any person             
-000600* obtaining a copy of this software and associated documentation          
-000700* files (the "Software"), to deal in the Software without                 
-000800* restriction, including without limitation the rights to use,            
-000900* copy, modify, merge, publish, distribute, sublicense, and/or            
-001000* sell copies of the Software, and to permit persons to whom the          
-001100* Software is furnished to do so, subject to the following                
-001200* conditions:                                                             
-001300*                                                                         
-001400* The above copyright notice and this permission notice shall be          
-001500* included in all copies or substantial portions of the Software.         
-001600*                                                                         
-001700* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,         
-001800* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES         
-001900* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                
-002000* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT             
-002100* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,            
-002200* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING            
-002300* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR           
-002400* OTHER DEALINGS IN THE SOFTWARE.                                         
-002500                                                                          
-002600 IDENTIFICATION DIVISION.                                                 
-002700 PROGRAM-ID. NAME.                                                        
-002800 AUTHOR. Andrew Young.                                                    
-002900 ENVIRONMENT DIVISION.                                                    
-003000 DATA DIVISION.                                                           
-003100                                                                          
-003200 WORKING-STORAGE SECTION.                                                 
-003300                                                                          
-003400 PROCEDURE DIVISION.                                                      
-003500                                                                          
-003600 PROGRAM-BEGIN.                                                           
-003700                                                                          
-003800 PROGRAM-DONE.                                                            
-003900   STOP RUN.                                                              
+000100* The MIT License (MIT)
+000200*
+000300* Copyright (c) 2015 Andrew Young
+000400*
+000500* Permission is hereby granted, free of charge, to any person
+000600* obtaining a copy of this software and associated documentation
+000700* files (the "Software"), to deal in the Software without
+000800* restriction, including without limitation the rights to use,
+000900* copy, modify, merge, publish, distribute, sublicense, and/or
+001000* sell copies of the Software, and to permit persons to whom the
+001100* Software is furnished to do so, subject to the following
+001200* conditions:
+001300*
+001400* The above copyright notice and this permission notice shall be
+001500* included in all copies or substantial portions of the Software.
+001600*
+001700* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+001800* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+001900* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+002000* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+002100* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+002200* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+002300* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+002400* OTHER DEALINGS IN THE SOFTWARE.
+002500
+002600 IDENTIFICATION DIVISION.
+002700 PROGRAM-ID. NAME.
+002800 AUTHOR. Andrew Young.
+002900 ENVIRONMENT DIVISION.
+003000 DATA DIVISION.
+003100
+003200 WORKING-STORAGE SECTION.
+003300
+003400 PROCEDURE DIVISION.
+003500
+003600 PROGRAM-BEGIN.
+003700
+003800 PROGRAM-DONE.
+003900   STOP RUN.
