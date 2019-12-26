b3f6db9f8cfc842bafa7255e0ebf5f9a62168c55
Andrew Young <andrew@vaelen.org>
2019-12-26 19:31:40 +0900

    Added screens, reworked file layout.

--- ledger/ledger.cob
+++ ledger/ledger.cob
@@ -33,74 +33,163 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
 
-           SELECT OPTIONAL COMPANY-FILE
-               ASSIGN TO "company.dat"
-               ORGANIZATION IS INDEXED
-               RECORD KEY IS COMPANY-ID
-               ACCESS MODE IS DYNAMIC.
-           
            SELECT OPTIONAL ACCOUNT-FILE
                ASSIGN TO "account.dat"
                ORGANIZATION IS INDEXED
                RECORD KEY IS ACCOUNT-ID
-               ALTERNATE RECORD KEY IS ACCOUNT-COMPANY-ID
                ACCESS MODE IS DYNAMIC.
 
            SELECT OPTIONAL LEDGER-FILE
                ASSIGN TO "ledger.dat"
                ORGANIZATION IS INDEXED
                RECORD KEY IS LEDGER-ID
-               ALTERNATE RECORD KEY IS LEDGER-ACCOUNT-ID
-               ALTERNATE RECORD KEY IS LEDGER-COMPANY-ID
+               ALTERNATE RECORD KEY IS LEDGER-ACCOUNT-ID WITH DUPLICATES
                ACCESS MODE IS DYNAMIC.
            
        DATA DIVISION.
        FILE SECTION.
-
-       FD  COMPANY-FILE.
-       01  COMPANY-RECORD.
-           05  COMPANY-ID                 PIC X(10).
-           05  COMPANY-NAME               PIC X(20).
        
        FD  ACCOUNT-FILE.
        01  ACCOUNT-RECORD.
-           05  ACCOUNT-ID.
-               10  ACCOUNT-COMPANY-ID     PIC X(10).
-               10  ACCOUNT-NUMBER         PIC X(20).
-           05  ACCOUNT-TYPE               PIC X(20).
-           05  ACCOUNT-DESCRIPTION        PIC X(50).
+           05  ACCOUNT-ID                 PIC 9(10) VALUE ZEROS.
+           05  ACCOUNT-COMPANY-NAME       PIC X(10) VALUE SPACES.
+           05  ACCOUNT-NUMBER             PIC X(20) VALUE SPACES.
+           05  ACCOUNT-TYPE               PIC X(20) VALUE SPACES.
+           05  ACCOUNT-DESCRIPTION        PIC X(50) VALUE SPACES.
+           05  ACCOUNT-STATUS             PIC X(1) VALUE SPACE.
 
        FD  LEDGER-FILE.
        01  LEDGER-RECORD.
-           05  LEDGER-ID                  PIC X(20).
-           05  LEDGER-ACCOUNT-ID.
-               10  LEDGER-COMPANY-ID      PIC X(10).
-               10  LEDGER-ACCOUNT-NUMBER  PIC X(20).
-           05  LEDGER-DESCRIPTION         PIC X(50).
-           05  LEDGER-AMOUNT              PIC S9(9)V9(2).
+           05  LEDGER-ID                  PIC 9(10) VALUE ZEROS.
+           05  LEDGER-ACCOUNT-ID          PIC 9(10) VALUE ZEROS.
+           05  LEDGER-DATE.
+               10  LEDGER-DATE-YEAR       PIC 9(4) VALUE ZEROS.
+               10  LEDGER-DATE-MONTH      PIC 9(2) VALUE ZEROS.
+               10  LEDGER-DATE-DAY        PIC 9(2) VALUE ZEROS.
+           05  LEDGER-DESCRIPTION         PIC X(50) VALUE SPACES.
+           05  LEDGER-AMOUNT              PIC S9(9)V9(2) VALUE ZEROS.
        
        WORKING-STORAGE SECTION.
-      
+
+       77  MENU-OPTION                    PIC 9(1) VALUE ZERO.
+       77  END-OF-FILE                    PIC X(1) VALUE SPACE.
+       77  NEXT-ACCOUNT-ID                PIC 9(10) VALUE ZEROS.
+
+       SCREEN SECTION.
+
+       01  MAIN-MENU-SCREEN.
+           05 BLANK SCREEN.
+           05 LINE 1 COL 1 VALUE "Main Menu:".
+           05 LINE 3 COL 3 VALUE "1) List Accounts".
+           05 LINE 4 COL 3 VALUE "2) Add Account".
+           05 LINE 5 COL 3 VALUE "3) Reports".
+           05 LINE 7 COL 3 VALUE "9) Exit".
+           05 LINE 9 COL 1 VALUE "Selecton => ".
+           05 LINE 9 COL 13 PIC Z USING MENU-OPTION AUTO.
+
+       01  REPORT-MENU-SCREEN.
+           05 BLANK SCREEN.
+           05 LINE 1 COL 1 VALUE "Report Menu:".
+           05 LINE 3 COL 3 VALUE "9) Exit".
+           05 LINE 5 COL 1 VALUE "Selecton => ".
+           05 LINE 5 COL 13 PIC Z USING MENU-OPTION AUTO.
+
+       01  ADD-ACCOUNT-SCREEN.
+           05  BLANK SCREEN.
+           05  LINE 1 COL 1 VALUE "Add Account:"
+           05  LINE 3 COL 1 VALUE "ID: ".
+           05  LINE 3 COL 5 PIC 9(10) FROM ACCOUNT-ID.
+           05  LINE 3 COL 20 VALUE "Company: ".
+           05  LINE 3 COL 30 PIC X(10) USING ACCOUNT-COMPANY-NAME.
+           05  LINE 3 COL 40 VALUE "Number: ".
+           05  LINE 3 COL 49 PIC X(20) USING ACCOUNT-NUMBER.
+           05  LINE 4 COL 1 VALUE "Type: ".
+           05  LINE 4 COL 7 PIC X(20) USING ACCOUNT-TYPE.
+           05  LINE 4 COL 20 VALUE "Status: ".
+           05  LINE 4 COL 29 PIC X(1) USING ACCOUNT-STATUS.
+           05  LINE 5 COL 1 VALUE "Description: ".
+           05  LINE 5 COL 14 PIC X(50) USING ACCOUNT-DESCRIPTION.
+       
        PROCEDURE DIVISION.
       
        PROGRAM-BEGIN.
-           OPEN I-O COMPANY-FILE.
            OPEN I-O ACCOUNT-FILE.
            OPEN I-O LEDGER-FILE.
            PERFORM MAIN-PROCESS.
-           CLOSE COMPANY-FILE.
-           CLOSE ACCOUNT-FILE.
            CLOSE LEDGER-FILE.
+           CLOSE ACCOUNT-FILE.
       
        PROGRAM-DONE.
             STOP RUN.
 
        MAIN-PROCESS.
-           PERFORM INIT-RECORDS.
+           MOVE "N" TO END-OF-FILE.
+           PERFORM INIT-ACCOUNT-RECORD.
+           PERFORM INIT-LEDGER-RECORD.
+           PERFORM MAIN-MENU.
 
-       INIT-RECORDS.
-           MOVE SPACE TO COMPANY-RECORD.
-           MOVE SPACE TO ACCOUNT-RECORD.
-           MOVE SPACE TO LEDGER-RECORD.
-           MOVE ZEROS TO LEDGER-AMOUNT.
-            
+       INIT-ACCOUNT-RECORD.
+           MOVE SPACES TO ACCOUNT-RECORD.
+           MOVE ZEROS TO ACCOUNT-ID.
+
+       INIT-LEDGER-RECORD.
+           MOVE ZEROS TO LEDGER-RECORD.
+           MOVE SPACES TO LEDGER-DESCRIPTION.
+
+       MAIN-MENU.
+           PERFORM MAIN-MENU-LOOP
+               UNTIL MENU-OPTION IS EQUAL TO 9.
+
+       MAIN-MENU-LOOP.
+           MOVE ZERO TO MENU-OPTION.
+           DISPLAY MAIN-MENU-SCREEN.
+           ACCEPT MAIN-MENU-SCREEN.
+           MOVE FUNCTION UPPER-CASE(MENU-OPTION) TO MENU-OPTION.
+           IF MENU-OPTION IS EQUAL TO 1
+               PERFORM LIST-ACCOUNTS
+           ELSE IF MENU-OPTION IS EQUAL TO 2
+               PERFORM ADD-ACCOUNTS
+           ELSE IF MENU-OPTION IS EQUAL TO 3
+               PERFORM REPORT-MENU.
+
+       LIST-ACCOUNTS.
+           DISPLAY "Accounts:".
+           MOVE "N" TO END-OF-FILE.
+           PERFORM RESET-ACCOUNT-FILE-POSITION.
+           IF END-OF-FILE IS NOT EQUAL TO "Y"
+               PERFORM READ-NEXT-ACCOUNT-RECORD
+               PERFORM DISPLAY-ACCOUNT-RECORD
+                   UNTIL END-OF-FILE IS EQUAL TO "Y"
+           END-IF.
+           DISPLAY " ".
+
+       RESET-ACCOUNT-FILE-POSITION.
+           MOVE ZEROS TO ACCOUNT-ID.
+           START ACCOUNT-FILE KEY IS GREATER THAN OR EQUAL TO ACCOUNT-ID
+               INVALID KEY MOVE "Y" TO END-OF-FILE.
+           
+       READ-NEXT-ACCOUNT-RECORD.
+           READ ACCOUNT-FILE NEXT RECORD
+               AT END MOVE "Y" TO END-OF-FILE.
+           
+       DISPLAY-ACCOUNT-RECORD.
+           
+
+       ADD-ACCOUNTS.
+           PERFORM INIT-ACCOUNT-RECORD.
+           MOVE NEXT-ACCOUNT-ID TO ACCOUNT-ID.
+           DISPLAY ADD-ACCOUNT-SCREEN.
+           ACCEPT ADD-ACCOUNT-SCREEN.
+           ADD 1 TO NEXT-ACCOUNT-ID.
+
+       REMOVE-ACCOUNT.
+                          
+       REPORT-MENU.
+           PERFORM REPORT-MENU-LOOP
+               UNTIL MENU-OPTION IS EQUAL TO 9.
+           MOVE ZERO TO MENU-OPTION.
+
+       REPORT-MENU-LOOP.
+           MOVE ZERO TO MENU-OPTION.
+           DISPLAY REPORT-MENU-SCREEN.
+           ACCEPT REPORT-MENU-SCREEN.
