e6385d07e46311c0c0bcbddc1e7d0441ea160ab1
Andrew Young <andrew@vaelen.org>
2020-01-03 18:08:59 +0900

    update copywrite line

--- ledger/ledger.cob
+++ ledger/ledger.cob
@@ -1,6 +1,6 @@
       * The MIT License (MIT)
       *
-      * Copyright (c) 2019 Andrew Young
+      * Copyright (c) 2019 Andrew C. Young
       *
       * Permission is hereby granted, free of charge, to any person
       * obtaining a copy of this software and associated documentation
